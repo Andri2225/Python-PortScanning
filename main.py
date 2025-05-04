06282edb7c2851c2666e1077fb8d2356bc940427
Andri <andri9825559.@gmail.com>
2025-05-04 15:00:20 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from socket import *
+import time 
+startTime = time.time()
+
+## Pemindaian port mirip dengan pergi ke rumah seseorang dan memeriksa pintu dan jendelanya.
+## Itulah mengapa disarankan untuk menggunakan pemindai port pada hos lokal atau situs web Anda sendiri (jika ada).
+print("""
+            
+            ███╗   ██╗███████╗████████╗██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗    ███████╗ █████╗ ███╗   ██╗███╗   ██╗██╗███╗   ██╗ ██████╗         
+            ████╗  ██║██╔════╝╚══██╔══╝██║    ██║██╔═══██╗██╔══██╗██║  ██║    ██╔════╝██╔══██╗████╗  ██║████╗  ██║██║████╗  ██║██╔════╝ 
+            ██╔██╗ ██║█████╗     ██║   ██║ █╗ ██║██║   ██║██████╔╝███████║    ███████╗███████║██╔██╗ ██║██╔██╗ ██║██║██╔██╗ ██║██║  ███╗
+            ██║╚██╗██║██╔══╝     ██║   ██║███╗██║██║   ██║██╔══██╗██╔══██║    ╚════██║██╔══██║██║╚██╗██║██║╚██╗██║██║██║╚██╗██║██║   ██║
+            ██║ ╚████║███████╗   ██║   ╚███╔███╔╝╚██████╔╝██║  ██║██║  ██║    ███████║██║  ██║██║ ╚████║██║ ╚████║██║██║ ╚████║╚██████╔╝
+            ╚═╝  ╚═══╝╚══════╝   ╚═╝    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝    ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚═╝╚═╝                                                         
+                                    
+
+                                                                      ..
+                               ,,,                         MM .M
+                           ,!MMMMMMM!,                     MM MM  ,.
+   ., .M                .MMMMMMMMMMMMMMMM.,          'MM.  MM MM .M'
+ . M: M;  M          .MMMMMMMMMMMMMMMMMMMMMM,          'MM,:M M'!M'
+;M MM M: .M        .MMMMMMMMMMMMMMMMMMMMMMMMMM,         'MM'...'M
+ M;MM;M :MM      .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.       .MMMMMMMM
+ 'M;M'M MM      MMMMMM  MMMMMMMMMMMMMMMMM  MMMMMM.    ,,M.M.'MMM'
+  MM'MMMM      MMMMMM @@ MMMMMMMMMMMMMMM @@ MMMMMMM.'M''MMMM;MM'
+ MM., ,MM     MMMMMMMM  MMMMMMMMMMMMMMMMM  MMMMMMMMM      '.MMM
+ 'MM;MMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMM
+  ''.'MMM'  .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM       MMMM
+   MMC      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMMM
+  .MM      :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM''MMM       MMMMM
+  MMM      :M  'MMMMMMMMMMMMM.MMMMM.MMMMMMMMMM'.MM  MM:M.    'MMMMM
+ .MMM   ...:M: :M.'MMMMMMMMMMMMMMMMMMMMMMMMM'.M''   MM:MMMMMMMMMMMM'
+AMMM..MMMMM:M.    :M.'MMMMMMMMMMMMMMMMMMMM'.MM'     MM''''''''''''
+MMMMMMMMMMM:MM     'M'.M'MMMMMMMMMMMMMM'.MC'M'     .MM
+ '''''''''':MM.       'MM!M.'M-M-M-M'M.'MM'        MMM
+            MMM.            'MMMM!MMMM'            .MM
+             MMM.             '''   ''            .MM'
+              MMM.                               MMM'
+               MMMM            ,.J.JJJJ.       .MMM'
+                MMMM.       'JJJJJJJ'JJJM   CMMMMM
+                  MMMMM.    'JJJJJJJJ'JJJ .MMMMM'
+                    MMMMMMMM.'  'JJJJJ'JJMMMMM'
+                      'MMMMMMMMM'JJJJJ JJJJJ'
+                         ''MMMMMMJJJJJJJJJJ'
+                                 'JJJJJJJJ'    
+
+            
+            """)
+if __name__ == '__main__':
+    target = input('Enter the host to be scanned: ')
+    t_IP = gethostbyname(target)
+    ##kamu bisa masukin ip atau nama website ke dalamnya.
+   
+    print("Memulai scanning pada host: ", t_IP)
+    
+    for i in range (50, 500):
+        s = socket(AF_INET, SOCK_STREAM)
+        
+        conn = s.connect_ex((t_IP, i))
+        if(conn == 0):
+            print("Port %d: OPEN" % (i,))
+        s.close()
+print("Waktu yang di gunakan:", time.time() - startTime)
